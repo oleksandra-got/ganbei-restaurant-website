7ac62b70f5ee74884f6a3e2b87b2259e28565c1c
Oleksandra <orlionaorliona@gmail.com>
2026-08-16 21:15:52 +0200

    JavaScript file was added

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Ganbei is working!");
+
+const reservationForm = document.querySelector("#reservation-modal");
+const bookbtn = document.querySelector(".btn"); //"BOOK A TABLE" button
+const closebtn = document.querySelector(".close-btn"); //X button
+const form = document.querySelector(reservation-form);
