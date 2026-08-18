15e140094ab1cf8e15fd757317ed96220746973f
Oleksandra <orlionaorliona@gmail.com>
2026-08-19 01:00:54 +0200

    Refactor modal functionality in script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ console.log("Ganbei is working!");
 const openModalBtn = document.getElementById('openModalBtn');
 const reservationForm = document.querySelector(".reservation-modal");
 const closeButton = document.querySelector(".close-btn");
-const form = document.querySelector("#reservation-form");
 
 openModalBtn.addEventListener("click", function(event) {
     event.preventDefault();
